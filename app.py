7ee5ec35a661c3f69452dfdc2b48033dc2c7014d
Aiden Yang <alyang250@gmail.com>
2014-06-23 21:23:55 -0700

    configuring port for heroku

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!flask/bin/python
+import os
 from flask import Flask, request, jsonify, abort
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
@@ -101,4 +102,5 @@ def deleteLink(id):
             return "No columns deleted"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)

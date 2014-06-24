80dc69d976d0616d6b25352075efb7986995dbd4
Aiden Yang <alyang250@gmail.com>
2014-06-23 20:30:39 -0700

    transitioning to cleardb

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,26 +11,92 @@ class Link(db.Model):
     __tablename__ = 'link'
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.Text)
+    author = db.Column(db.Text)
     url = db.Column(db.Text)
     type = db.Column(db.Integer)
     date_added = db.Column(db.DateTime)
 
+    def __init__(self, title, author, url, type):
+        self.title = title
+        self.url = url
+        self.author = author
+        self.type = type
+
+    def __unicode__(self):
+        return '<Link %r>' %self.title
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'url': self.url,
+            'type': self.type,
+            'date_added': self.date_added
+        }
+
+
 @app.route('/links', methods=['GET'])
-def links():
+def getAllLinks():
     if request.method == 'GET':
-        results = Link.query.limit(10).offset(0).all()
+        lim = request.args.get('limit', 10)
+        off = request.args.get('offset', 0)
+        results = Link.query.limit(lim).offset(off).all()
 
         json_results = []
         for result in results:
             d = {
                     'id': result.id,
                     'title': result.title,
+                    'author': result.author,
                     'url': result.url,
                     'type': result.type,
                     'date_added' : result.date_added
                 }
             json_results.append(d)
         return jsonify(items=json_results)
+
+@app.route('/links/<int:id>', methods=['GET'])
+def getLinkById(id):
+    if request.method == 'GET':
+        result = Link.query.filter_by(id=id).first()
+        if not result:
+            abort(404)
+        else:
+            json_result = {
+            'id': result.id,
+            'title': result.title,
+            'url': result.url,
+            'author': result.author,
+            'type': result.type,
+            'date_added': result.date_added
+            }
+            return jsonify(items=json_result)
+
+
+
+@app.route('/links', methods=['POST'])
+def postLink():
+    if request.method == 'POST':
+        title = request.form['title']
+        url = request.form['url']
+        author = request.form['author']
+        type = request.form['type']
+        link = Link(title, url, type)
+        db.session.add(link)
+        db.session.commit()
+        return jsonify({'link' : link.serialize()}), 201
+
+
+@app.route('/links/<int:id>', methods=['DELETE'])
+def deleteLink(id):
+    if request.method == 'DELETE':
+        deleted = Link.query.filter(Link.id==id).delete()
+        db.session.commit()
+        if deleted == 0:
+            return "{0} column(s) deleted".format(deleted)
+        else:
+            return "No columns deleted"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- nameless/test.js
+++ nameless/test.js
@@ -1,8 +1,19 @@
+var url = "http://localhost:5000/links";
+
 chrome.tabs.getSelected(function(tab) {
-	$('.url').append(tab.url);
+	$('#url').val(tab.url);
 });
 
 $( "#add" ).submit(function( event ) {
   $('.url').replaceWith('<p>Submitted</p>');
   event.preventDefault();
+  $.ajax({
+	type: "POST",
+	url: url,
+	data: $('#add').serialize(),
+	success: function(data) {
+		alert(data);
+	}
 });
+});
+
